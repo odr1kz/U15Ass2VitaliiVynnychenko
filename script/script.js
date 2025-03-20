3894f04453c166e9bc928c44691ba3f639fd0113
Vitalii Vynnychenko <vitalii.vynnych65@gmail.com>
2025-03-20 22:34:47 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
+    menu.classList.toggle('ri');
     navlist.classList.toggle('open');
 };
 
