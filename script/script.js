9a72de33be9d6ab042b0910bdb6456e1f59e36ea
Vitalii Vynnychenko <vitalii.vynnych65@gmail.com>
2025-03-21 15:46:01 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -14,6 +14,28 @@ menu.onclick = () => {
     navlist.classList.toggle('open');
 };
 
+document.querySelectorAll('.navlist a').forEach(link => {
+    link.addEventListener('mouseenter', function(e) {
+        const width = link.offsetWidth;
+        const offsetX = e.clientX - link.getBoundingClientRect().left;
+        
+        let origin;
+        if (offsetX < width / 3) {
+            origin = 'left';
+        } else if (offsetX > (2 * width) / 3) {
+            origin = 'right';
+        } else {
+            origin = 'center';
+        }
+        
+        link.style.setProperty('--transform-origin', origin);
+    });
+
+    link.addEventListener('mouseleave', function() {
+        link.style.setProperty('--transform-origin', 'center');
+    });
+});
+
 const sr = ScrollReveal ({
     distance: '65px',
     duration: 2600,
@@ -24,3 +46,27 @@ const sr = ScrollReveal ({
 sr.reveal('.hero-text', {delay:200, origin:'top'});
 sr.reveal('.hero-img', {delay:450, origin:'top'});
 sr.reveal('.icons', {delay:500, origin:'left'});
+
+
+//Javascript for video slider nav
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    })
+});
